67cdcd63bf8cc1a9a16eb267dc38858bb42ba36d
かずき <skaz0110@yahoo.co.jp>
2020-12-09 22:52:54 +0900

    Q15、Q16を回答

--- task.rb
+++ task.rb
@@ -144,6 +144,18 @@ def q15
   data2 = { name: "yamada", hobby: "baseball", role: "normal" }
 
   # 以下に回答を記載
+  
+    if data1.key?(:age)
+      puts "OK"
+    else
+      puts "NG"
+    end
+
+    if data2.key?(:age)
+      puts "OK"
+    else
+      puts "NG"
+    end  
 
 end
 
@@ -156,6 +168,9 @@ def q16
   ]
 
   # 以下に回答を記載
+  users.each do |user|
+     puts "私の名前は#{user[:name]}です。年齢は#{user[:age]}歳です"
+  end
 
 end
 
